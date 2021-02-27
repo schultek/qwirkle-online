3ef50a9e4952501661031111a0be9e728789ada9
Kilian Schulte <schultek@in.tum.de>
2021-02-27 15:47:58 +0100

    improve waiting area

--- lib/screens/game/game_screen.dart
+++ lib/screens/game/game_screen.dart
@@ -1,3 +1,4 @@
+import 'package:clipboard/clipboard.dart';
 import 'package:flutter/material.dart';
 import 'package:tuple/tuple.dart';
 
@@ -89,19 +90,53 @@ class GameScreenState extends State<GameScreen> with TickerProviderStateMixin {
           ),
         ),
         const SizedBox(height: 20),
-        ListView.builder(
-          shrinkWrap: true,
-          itemCount: game.players.value.length,
-          itemBuilder: (context, index) {
-            return Text(
-              game.players.value[index].nickname,
-              textAlign: TextAlign.center,
-              style: const TextStyle(
-                color: Colors.white,
-                fontSize: 20,
+        ...ListTile.divideTiles(
+          context: context,
+          tiles: game.players.value.map((player) {
+            return IntrinsicWidth(
+              child: ListTile(
+                contentPadding: const EdgeInsets.symmetric(vertical: 5),
+                leading: CircleAvatar(
+                  backgroundColor: SymbolPainter.colorFromTag(player.color),
+                  radius: 18,
+                  child: Text(
+                    player.nickname[0],
+                    style: const TextStyle(fontSize: 24, color: Colors.white, fontWeight: FontWeight.bold),
+                  ),
+                ),
+                title: Text(player.nickname, style: const TextStyle(color: Colors.white, fontWeight: FontWeight.bold)),
               ),
             );
+          }),
+          color: Colors.white38,
+        ),
+        const SizedBox(height: 40),
+        OutlinedButton(
+          onPressed: () async {
+            await FlutterClipboard.copy(Uri.base.toString().replaceFirst("/game/", "/join/"));
+            ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+              backgroundColor: Colors.green,
+              content: Text("In Zwischenablage kopiert"),
+            ));
           },
+          style: ButtonStyle(
+            padding: MaterialStateProperty.all(const EdgeInsets.symmetric(vertical: 15, horizontal: 20)),
+            side: MaterialStateProperty.all(const BorderSide(color: Colors.blue)),
+            shape: MaterialStateProperty.all(RoundedRectangleBorder(borderRadius: BorderRadius.circular(20))),
+            foregroundColor: MaterialStateProperty.all(Colors.blue),
+          ),
+          child: Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text(Uri.base.toString().replaceFirst("/game/", "/join/"), style: const TextStyle(color: Colors.blue)),
+              const SizedBox(width: 10),
+              const Icon(
+                Icons.copy,
+                color: Colors.blue,
+                size: 15,
+              ),
+            ],
+          ),
         ),
         const SizedBox(height: 40),
         if (game.isGameMaster)
